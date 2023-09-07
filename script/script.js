0bce10e6b267ff9432e46ddc02d0ea32b7507517
Thybow <tmilville.pro@gmail.com>
2023-09-07 21:27:17 +0200

    start js file

---
+++ script/script.js
@@ -0,0 +1,22 @@
+class WeatherAppLogic {
+  constructor() {
+    // Structure
+    this.city = document.getElementById('city');
+
+    // API - WeatherStack
+    this.apiKey = 'f71ba1ecf0a2fe236a840990b40069a4';
+  }
+
+  // Methods
+  load() {
+  }
+}
+
+class WeatherAppIntegration {
+  constructor() {
+  }
+
+  // Methods
+  load() {
+  }
+}
